a2068afa173bead8d6c6d4053a8bcac0855e01a0
Adam Prasetia <adam.prasetia@kompas.com>
2023-12-17 12:21:22 +0700

    update produk detail page

--- application/views/detail_view.php
+++ application/views/detail_view.php
@@ -4,18 +4,33 @@
 	<div class="m-4 text-center">
 		<h1><?php echo $item->name ?></h1>
 	</div>
-	<?php if(!empty($item->video_id)): ?>
-	<div>
-		<div class="embed-responsive embed-responsive-16by9">
-			<iframe width="100%" height="315" src="https://www.youtube.com/embed/<?php echo $item->video_id ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-6">
+			<img class="img-fluid" src="<?php echo base_url($item->image)?>" alt="">
+		</div>
+		<div class="col-md-6">
+			<?php echo $item->desc ?>
+			<div class="mt-2 text-center">
+				<?php if($item->pdf):?>
+				<a download href="<?php echo base_url('assets/pdf/'.$item->pdf) ?>" class="btn btn-danger">Donwload PDF</a>
+				<?php endif ?>
+				<a href="https://api.whatsapp.com/send?phone=<?php echo $store->phone ?>&text=<?php echo config_item('wa') ?>" class="btn btn-danger">Info Lebih Lanjut</a>
+			</div>
+
 		</div>
 	</div>
-	<?php endif ?>
-	<?php
-		$gallery = json_decode($item->gallery);
-		if(!empty($gallery)):
-	?>
-
+</div>
+<?php
+	$gallery = json_decode($item->gallery);
+	if(!empty($gallery)):
+?>
+<div class="container">
+	<div class="m-4 text-center">
+		<h2>Gallery</h2>
+	</div>
 	<div id="carouselExampleIndicators" class="carousel slide mt-2" data-ride="carousel">
 		<ol class="carousel-indicators">
 			<?php foreach ($gallery as $key => $value) { ?>		
@@ -38,19 +53,21 @@
 			<span class="sr-only">Next</span>
 		</a>
 	</div>	
-	<?php endif ?>
-
-	<?php if($item->pdf):?>
-	<div class="mt-2 text-center">
-	<a download href="<?php echo base_url('assets/pdf/'.$item->pdf) ?>" class="btn btn-danger">Donwload PDF</a>
+</div>
+<?php endif ?>
+<?php if(!empty($item->video_id)): ?>
+<div class="container">
+	<div class="m-4 text-center">
+		<h2>Video</h2>
 	</div>
-	<?php endif ?>
-	<div class="mt-2 text-center">
-		<?php echo $item->desc ?>
+	<div>
+		<div class="embed-responsive embed-responsive-16by9">
+			<iframe width="100%" height="315" src="https://www.youtube.com/embed/<?php echo $item->video_id ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+		</div>
 	</div>
-	<div class="mt-2 text-center">
-		<img class="img-fluid" src="<?php echo base_url($item->image)?>" alt="">
-	</div>
+</div>
+<?php endif ?>
+<div class="container">
 	<div class="mt-2 text-center">
 		<a href="https://api.whatsapp.com/send?phone=<?php echo $store->phone ?>&text=<?php echo config_item('wa') ?>" class="btn btn-danger">Info Lebih Lanjut</a>
 	</div>
