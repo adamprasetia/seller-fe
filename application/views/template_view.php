9fb990482097e6aa7b74ce4d73282161a079910d
Adam Prasetia <adam.prasetia@kompas.com>
2023-12-17 08:59:10 +0700

    fix icon

--- application/views/template_view.php
+++ application/views/template_view.php
@@ -38,7 +38,7 @@
 
 
         <!-- For favicon png -->
-		<link rel="shortcut icon" type="image/icon" href="<?php echo base_url('assets/logo/favicon.ico"') ?>"/>
+		<link rel="shortcut icon" type="image/icon" href="<?php echo base_url($store->icon) ?>"/>
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">       
 		<link rel="stylesheet" href="<?php echo base_url('assets/css/style.css') ?>">       
 		<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/lozad/dist/lozad.min.js"></script>
